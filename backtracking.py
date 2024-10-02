b8a90ff89a5cd06632b81cbac187141ab93a4621
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2024-10-02 13:13:27 +0000

    Check Collumn function added

--- backtracking.py
+++ backtracking.py
@@ -1,7 +1,8 @@
 """"
     Code to solve a Sudoku Puzzle using the backtracking algorithm.
     Input:
-        board = [
+"""
+board = [
             [5, 3, 0, 0, 7, 0, 0, 0, 0],
             [6, 0, 0, 1, 9, 5, 0, 0, 0],
             [0, 9, 8, 0, 0, 0, 0, 6, 0],
@@ -12,6 +13,7 @@
             [0, 0, 0, 4, 1, 9, 0, 0, 5],
             [0, 0, 0, 0, 8, 0, 0, 7, 9]
         ]
+"""
     In this input, the 0s represent the numbers that are missing from the puzzle.
     
     Written by Rodrigo Almeida (rpa299)    
@@ -27,18 +29,20 @@ def printBoard(board):
 #check row
 #   true if valid
 #   false if invalid
-def checkRow(board, row, col, valueAttempt):
-    for i in range(9):
-        if(i != col):
-            if(valueAttempt == board[row][i]):
-                return False
+def checkRow(board, row, valueAttempt):
+    for col in range(9):
+        if(valueAttempt == board[row][col]):
+            return False
     return True
-
 
 #check coll
 #   true if value
 #   false if invalid
-
+def checkCol(board, col, valueAttempt):
+    for row in range(9):
+        if(valueAttempt == board[row][col]):
+            return False
+    return True
 
 #check block
 #   true if valid
